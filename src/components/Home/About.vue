26bd3c131c2600031d683528d6930215ca69f292
alphacentauri82 <rm82dc@gmail.com>
2019-01-25 23:01:33 -0700

    adding GCP GDE badge

--- src/components/Home/About.vue
+++ src/components/Home/About.vue
@@ -2,7 +2,7 @@
     <section id="about">
 		<div class="container">	
 			<h3>About (Bio)</h3>
-			<p><span class="image left"><img src="@/assets/images/avatar2.jpg" alt="" /></span>Diana Rodriguez is a Full Stack Developer/DevOps, Vue Vixens Worldwide Community Organiser, Google Developer Expert in Web Technologies and Auth0 Ambassador. She was also a NativeScript-Vue Ambassador for 2018. <p> With 18 years' experience and a strong background in back end and infrastructure, Diana likes to bring together the best of both worlds, spreading DevOps culture. As a Chief DevOps Architect She has a passion for Automation, IaaS and containers being very proficient in Kubernetes and Docker. <p>
+			<p><span class="image left"><img src="@/assets/images/avatar2.jpg" alt="" /></span>Diana Rodriguez is a Full Stack Developer/DevOps, Vue Vixens Worldwide Community Organiser, Google Developer Expert in Web Technologies and Google Cloud Platform. Auth0 Ambassador. She was also a NativeScript-Vue Ambassador for 2018. <p> With 18 years' experience and a strong background in back end and infrastructure, Diana likes to bring together the best of both worlds, spreading DevOps culture. As a Chief DevOps Architect She has a passion for Automation, IaaS and containers being very proficient in Kubernetes and Docker. <p>
 			She's super enthusiastic about everything to encourage people to start a career in development and a fan of female devs and DevOps. In her spare time she likes to play videogames, also a huge fan of sports, Diana is a rugby player and taekwondo black belt.</p>
 		    <h5>In fewer words:</h5>
 			<p></p>
